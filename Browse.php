c24618cad905e548106dbd0db4eb62d55d91ae04
chrisfoong <chrisfoong009@gmail.com>
2023-10-09 17:52:15 +0700

    GOOD

---
+++ Browse.php
@@ -0,0 +1,66 @@
+<?php 
+    require_once('componets/navbar.php');
+<?
+
+
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KenshoCanabis</title>
+    <link rel="stylesheet" href="webstyle.css">
+    <link rel="stylesheet" href="webstylertwo.css">
+</head>
+
+<body>
+    <?php 
+        require_once("components/navbar.php"); 
+    ?>
+
+        <section class="products">
+            <h1>Our Products</h1>
+            <div class="all-products">
+                <div class="product">
+                    <br><img src="img/aaa.jpg">
+                    <div class="product-info">
+                        <h4 class="product-title">100g Weed
+                        </h4>
+                        <p class="product-price"></p>750฿
+                        <br><a class="product-btn" href="#">Buy now!</a>
+
+
+                    </div>
+                </div>
+
+                <div class="product">
+                    <br><img src="img/aaa.jpg">
+                    <div class="product-info">
+                        <h4 class="product-title">plant for growing
+                        </h4>
+                        <p class="product-price"></p>1000฿
+                        <br><a class="product-btn" href="#">Buy now!</a>
+
+
+                    </div>
+                </div>
+
+                <div class="product">
+                    <br><img src="img/aaa.jpg">
+                    <div class="product-info">
+                        <h4 class="product-title">Lighter
+                        </h4>
+                        <p class="product-price"></p>10,000฿
+                        <br><a class="product-btn" href="#">Buy now!</a>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+    </div>
+</body>
---
+++ components/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+    <div class="navbar">
+            <img src="img/weedlogoreal.png" class="logo">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">Location</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">HELP</a></li>
+                    <img src="img/American-removebg-preview.png" class="lang">
+
+                </ul>
+    </div>   
+</nav>
