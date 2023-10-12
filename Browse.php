ac9df08bd6fca24829df940538be04854d87e946
chrisfoong <chrisfoong009@gmail.com>
2023-10-12 21:14:25 +0700

    ThirdCommit

--- Browse.php
+++ Browse.php
@@ -1,61 +1,61 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>KenshoCanabis</title>
-    <link rel="stylesheet" href="webstyle.css">
-    <link rel="stylesheet" href="webstylertwo.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>KenshoCanabis</title>
+  <link rel="stylesheet" type="text/css" href="./css/webstyle.css?<?php echo time(); ?>" />
+  <link rel="stylesheet" type="text/css" href="./css/webstylertwo.css?<?php echo time(); ?>" />
 </head>
 
 <body>
-    <div class ="banner">
-    <?php 
-        require_once("./components/navbar.php");
+  <div class="banner">
+    <?php
+    require_once("./components/navbar.php");
     ?>
 
-        <section class="products">
-            <h1>Our Products</h1>
-            <div class="all-products">
-                <div class="product">
-                    <br><img src="img/aaa.jpg">
-                    <div class="product-info">
-                        <h4 class="product-title">100g Weed
-                        </h4>
-                        <p class="product-price"></p>750฿
-                        <br><a class="product-btn" href="#">Buy now!</a>
+    <section class="products">
+      <h1>Our Products</h1>
+      <div class="all-products">
+        <div class="product">
+          <br><img src="img/aaa.jpg">
+          <div class="product-info">
+            <h4 class="product-title">100g Weed
+            </h4>
+            <p class="product-price"></p>750฿
+            <br><a class="product-btn" href="#">Buy now!</a>
 
 
-                    </div>
-                </div>
+          </div>
+        </div>
 
-                <div class="product">
-                    <br><img src="img/aaa.jpg">
-                    <div class="product-info">
-                        <h4 class="product-title">plant for growing
-                        </h4>
-                        <p class="product-price"></p>1000฿
-                        <br><a class="product-btn" href="#">Buy now!</a>
-
-
-                    </div>
-                </div>
-
-                <div class="product">
-                    <br><img src="img/aaa.jpg">
-                    <div class="product-info">
-                        <h4 class="product-title">Lighter
-                        </h4>
-                        <p class="product-price"></p>10,000฿
-                        <br><a class="product-btn" href="#">Buy now!</a>
+        <div class="product">
+          <br><img src="img/aaa.jpg">
+          <div class="product-info">
+            <h4 class="product-title">plant for growing
+            </h4>
+            <p class="product-price"></p>1000฿
+            <br><a class="product-btn" href="#">Buy now!</a>
 
 
-                    </div>
-                </div>
+          </div>
+        </div>
 
-            </div>
-        </section>
-    </div>
-    </div>
-    </div>
+        <div class="product">
+          <br><img src="img/aaa.jpg">
+          <div class="product-info">
+            <h4 class="product-title">Lighter
+            </h4>
+            <p class="product-price"></p>10,000฿
+            <br><a class="product-btn" href="#">Buy now!</a>
+
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </div>
+  </div>
+  </div>
 </body>
--- Index.php
+++ Index.php
@@ -1,28 +1,32 @@
 <html>
+
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>KenshoCanabis</title>
-        <link rel="stylesheet" href="webstyle.css">
-        <link rel="stylesheet" href="webstylertwo.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>KenshoCanabis</title>
+  <link rel="stylesheet" type="text/css" href="./css/webstyle.css?<?php echo time(); ?>" />
+  <link rel="stylesheet" type="text/css" href="./css/webstylertwo.css?<?php echo time(); ?>" />
 </head>
-    <body>
-        <div class="banner">
-    <?php 
-        require_once("components/navbar.php"); 
+
+<body>
+  <div class="banner">
+    <?php
+    require_once("components/navbar.php");
     ?>
-                
-        <div class="content">
-            <h1>BEST CANABIS IN THAILAND</h1>
-            <p>Welcome to our premium online dispensary, where we take pride in offering the finest selection of high-quality cannabis products available.<br>We are dedicated to providing a seamless and enjoyable experience for both seasoned connoisseurs and newcomers to the world of cannabis.</p>
-            <div>
-                
-                <button class="b" type="button"><span></span><a href="./Browse.php">BROWSE</a></button>
-                <button class="b" type="button"><span></span>MERCHS</button>
-                </ul>
-            </div>
-            </div>
-            
-        </div> 
-        </div>
-    </body>
+
+    <div class="content">
+      <h1>BEST CANABIS IN THAILAND</h1>
+      <p>Welcome to our premium online dispensary, where we take pride in offering the finest selection of high-quality
+        cannabis products available.<br>We are dedicated to providing a seamless and enjoyable experience for both
+        seasoned connoisseurs and newcomers to the world of cannabis.</p>
+      <div>
+
+        <button type="button"><span></span><a href="./Browse.php">BROWSE</a></button>
+        <button type="button"><span></span>MERCHS</button>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+  </div>
+</body>
